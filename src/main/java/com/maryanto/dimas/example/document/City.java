6e15949f260ebeeea870fd929eeaa5ae7f853a27
dimasm93 <software.dimas_m@icloud.com>
2020-10-07 15:14:53 +0700

    using indexes

--- src/main/java/com/maryanto/dimas/example/document/City.java
+++ src/main/java/com/maryanto/dimas/example/document/City.java
@@ -3,6 +3,7 @@ package com.maryanto.dimas.example.document;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,6 +14,7 @@ public class City {
 
     @Id
     private String id;
+    @Indexed
     private String code;
     private String name;
     @DBRef
--- src/main/java/com/maryanto/dimas/example/document/Province.java
+++ src/main/java/com/maryanto/dimas/example/document/Province.java
@@ -3,6 +3,7 @@ package com.maryanto.dimas.example.document;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -12,6 +13,7 @@ public class Province {
 
     @Id
     private String id;
+    @Indexed
     private String code;
     private String name;
 }
